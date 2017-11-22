c021a4f6aa5d40d0e3be3dbde941565a4b52471e
sylwia <vegoria@gmail.com>
2017-11-22 21:53:58 +0100

    added UserFriendKey

---
+++ MobileDuck/app/src/main/java/com/example/sylwia/mobileduck/repository/models/UserFriendKey.java
@@ -0,0 +1,23 @@
+package com.example.sylwia.mobileduck.repository.models;
+
+/**
+ * Created by vegor on 22.11.2017.
+ */
+
+public class UserFriendKey {
+    private long userId;
+    private long friendId;
+
+    public UserFriendKey(long userId, long friendId){
+        this.userId = userId;
+        this.friendId = friendId;
+    }
+
+    public long getUserId(){
+        return userId;
+    }
+
+    public long getFriendId(){
+        return friendId;
+    }
+}
