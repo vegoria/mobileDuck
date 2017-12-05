6cfd6b2827ad9421afc4df267cd7dbe31b0f00cc
sylwia <vegoria@gmail.com>
2017-12-05 21:02:32 +0100

    moving to listView after adding

--- MobileDuck/app/src/main/java/com/example/sylwia/mobileduck/AddNewShoplistActivity.java
+++ MobileDuck/app/src/main/java/com/example/sylwia/mobileduck/AddNewShoplistActivity.java
@@ -1,23 +1,39 @@
 package com.example.sylwia.mobileduck;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.sylwia.mobileduck.db.Manager;
+import com.example.sylwia.mobileduck.db.tables.ShoppingList;
+
+import java.util.List;
 
 public class AddNewShoplistActivity extends AppCompatActivity {
-
+    private ShoppingList shoppingList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_shoplist);
+        setOnClickListenerForAddingButton();
     }
 
-    void onClickAddNewShoplist(View view){
+    private void setOnClickListenerForAddingButton(){
+        Button button = (Button) findViewById(R.id.addShoplistButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickAddNewShoplist();
+            }
+        });
+    }
+
+    void onClickAddNewShoplist(){
         Thread thread = new Thread(new Runnable() {
 
             @Override
@@ -27,11 +43,22 @@ public class AddNewShoplistActivity extends AppCompatActivity {
                 SharedPreferences sharedPref = getApplicationContext().getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
                 String userName =  sharedPref.getString(getString(R.string.preference_user_login),"");
                 manager.addShoppingList(shopListName.getText().toString(),userName);
-                finish();
+                shoppingList = manager.getShoppingList(shopListName.getText().toString(), userName);
             }
         });
 
         thread.start();
+        try
+        {
+            thread.join();
+        }
+        catch(InterruptedException e)
+        {
+        }
+        Intent newActivityIntent=new Intent(getApplicationContext(),
+                ShopListActivity.class).putExtra("ListId",
+                shoppingList.getId()).putExtra("OwnList", true);
+        startActivity(newActivityIntent);
     }
 
 }
