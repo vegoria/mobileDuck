2b6160202ac75b7a40163414172451c8bb6dc7bb
pakopako <pawciopako@gmail.com>
2017-12-05 14:15:18 +0100

    fixed app crash on add friend button; adding friends works correctly now

--- MobileDuck/app/src/main/java/com/example/sylwia/mobileduck/AddNewFriendActivity.java
+++ MobileDuck/app/src/main/java/com/example/sylwia/mobileduck/AddNewFriendActivity.java
@@ -2,12 +2,11 @@ package com.example.sylwia.mobileduck;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
 
 import com.example.sylwia.mobileduck.db.Manager;
 import com.example.sylwia.mobileduck.db.tables.User;
@@ -17,47 +16,63 @@ import java.util.List;
 
 public class AddNewFriendActivity extends AppCompatActivity {
     private List<User> allUsers;
+    private Manager manager;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_friend);
-        allUsers =  new ArrayList<User>();
+        initData();
 
-                Thread thread = new Thread(new Runnable() {
-
+        Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                Manager manager = new Manager();
                 allUsers = manager.getAllUsers();
-                Spinner spinner = (Spinner) findViewById(R.id.spinner2);
-                ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, allUsers);
-                spinner.setAdapter(arrayAdapter);
             }
         });
-
         thread.start();
 
+        joinThread(thread);
+        setSpinnerAdapter();
     }
 
-    void onClickAddFriend(View view){
+    private void initData() {
+        allUsers =  new ArrayList<>();
+        manager = new Manager();
+        spinner = findViewById(R.id.spinner2);
+    }
+
+    private void setSpinnerAdapter() {
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_spinner_item, allUsers);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+    }
+
+    private void joinThread(Thread thread) {
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onClickAddFriend(View view){
         Thread thread = new Thread(new Runnable() {
 
             @Override
             public void run() {
-                Manager manager = new Manager();
-                Spinner spinner = (Spinner) findViewById(R.id.spinner2);
                 SharedPreferences sharedPref = getApplicationContext().getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
                 String userName =  sharedPref.getString(getString(R.string.preference_user_login),"");
                 String friendName = spinner.getSelectedItem().toString().replaceAll("[^a-zA-Z0-9]", "");
                 manager.addFriend(userName, friendName);
             }
         });
-
         thread.start();
+        joinThread(thread);
     }
 
-    void onClickCancel(View view){
+    public void onClickCancel(View view){
         finish();
     }
 }
